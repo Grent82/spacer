d4770a5fdfa82e211e41aafbd3f3747ab8c6ba7b
André Dittrich <andre.dittrich@adesso.de>
2026-02-23 16:41:18 +0100

    updates personality enum

--- src/Spacer.Domain/Enums/PersonalityType.cs
+++ src/Spacer.Domain/Enums/PersonalityType.cs
@@ -3,16 +3,16 @@ namespace Spacer.Domain.Enums;
 public enum PersonalityType
 {
     Unknown = 0,
-    Tiger = 11,
-    Monkey = 12,
-    Koala = 13,
-    Wolf = 14,
-    Pegasus = 21,
-    Elephant = 22,
-    Lion = 23,
-    Cheetah = 24,
-    BlackLeopard = 31,
-    Sheep = 32,
-    Fawn = 41,
-    RaccoonDog = 42,
+    Tiger = 11, // stark, stolz, direkt, pflichtbewusst; negativ: stur, dominant, ungeduldig
+    Monkey = 12, // kommunikativ, flexibel, neugierig, schnell; negativ: sprunghaft, unruhig, oberflaechlich
+    Koala = 13, // ruhig, ausgeglichen, stabil, beharrlich; negativ: bequem, langsam, veraenderungsresistent
+    Wolf = 14, // eigenstaendig, analytisch, individualistisch, reserviert; negativ: distanziert, kritisch, eigensinnig
+    Pegasus = 21, // idealistisch, sensibel, kreativ, freiheitsliebend; negativ: launisch, unrealistisch, schwer greifbar
+    Elephant = 22, // zuverlaessig, geduldig, verantwortungsbewusst, ausdauernd; negativ: stur, zaeh, konservativ
+    Lion = 23, // fuehrungsstark, ehrgeizig, grosszuegig, stolz; negativ: autoritaer, eitel, ueberheblich
+    Cheetah = 24, // zielorientiert, schnell, leistungsgetrieben, ungeduldig; negativ: hektisch, ruhelos, reizbar
+    BlackLeopard = 31, // cool, elegant, anpassungsfaehig, taktisch; negativ: berechnend, distanziert, schwer zu lesen
+    Sheep = 32, // kooperativ, freundlich, harmoniebeduerftig, vorsichtig; negativ: unsicher, konfliktscheu, abhaengig
+    Fawn = 41, // sensibel, loyal, warmherzig, verletzlich; negativ: aengstlich, nachgiebig, verletztlich
+    RaccoonDog = 42, // gesellig, praktisch, bodenstaendig, genussorientiert; negativ: bequem, inkonsequent, konfliktscheu
 }
